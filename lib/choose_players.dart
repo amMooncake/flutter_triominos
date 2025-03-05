17d099becd5a14d2e3c51c9ff4ff0e0449c1c45b
alekm <alekmalawski123@gmail.com>
2025-03-05 17:22:45 +0100

    Add player management functionality with dynamic input fields

--- lib/choose_players.dart
+++ lib/choose_players.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'models/player.dart';
 
@@ -12,19 +11,42 @@ class ChoosePlayersScreen extends StatefulWidget {
 
 class _ChoosePlayersScreenState extends State<ChoosePlayersScreen> {
   int _numbersOfPlayers = 0;
-  List<MyPlayer> players = [];
   final _formKey = GlobalKey<FormState>();
+  List<TextEditingController> _controllers = [];
 
   void addPlayer() {
     if (_numbersOfPlayers < 6) {
       _numbersOfPlayers++;
-      players.add(MyPlayer(name: '', color: Colors.blue));
+      _controllers.add(TextEditingController());
     } else {
       ScaffoldMessenger.of(context).clearSnackBars();
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Maximum Players Reached')));
     }
     setState(() {});
-    print(players);
+  }
+
+  @override
+  void dispose() {
+    for (var controller in _controllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  void removePlayer() {
+    _numbersOfPlayers--;
+    _controllers.removeLast();
+    setState(() {});
+  }
+
+  void StartGame() {
+    if (_formKey.currentState!.validate()) {
+      List<MyPlayer> players = [];
+      for (var controller in _controllers) {
+        players.add(MyPlayer(name: controller.text, color: Colors.primaries[_controllers.indexOf(controller)]));
+      }
+      print(players);
+    }
   }
 
   @override
@@ -71,73 +93,91 @@ class _ChoosePlayersScreenState extends State<ChoosePlayersScreen> {
                           ),
                         ],
                       ),
-                      SizedBox(height: _numbersOfPlayers == 0 ? 0 : 50),
+                      SizedBox(height: _numbersOfPlayers == 0 ? 0 : 30),
                       if (_numbersOfPlayers != 0)
                         Form(
                           key: _formKey,
                           child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
                             children: [
-                              ListView.separated(
-                                separatorBuilder: (BuildContext ctxt, int index) => SizedBox(
-                                  height: 20,
-                                ),
-                                itemCount: _numbersOfPlayers,
+                              GridView.builder(
+                                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10, childAspectRatio: 3 / 2),
                                 shrinkWrap: true,
-                                itemBuilder: (BuildContext ctxt, int index) {
-                                  return Material(
-                                    elevation: 2,
-                                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                                    color: Colors.white,
+                                itemCount: _numbersOfPlayers,
+                                itemBuilder: (context, index) {
+                                  return SizedBox(
+                                    height: 10,
                                     child: Padding(
                                       padding: EdgeInsets.all(10),
-                                      child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          SizedBox(
-                                            width: 130,
-                                            child: TextFormField(
-                                              maxLength: 10,
-                                              decoration: InputDecoration(
-                                                counterText: "",
-                                                hintText: 'Enter a name',
-                                                hintStyle: TextStyle(color: Colors.grey),
-                                                enabledBorder: InputBorder.none, // No border when field is enabled
-                                                // Optional: to use the primary color for the cursor and text
-                                              ),
-                                              style: TextStyle(color: Colors.black),
-                                            ),
+                                      child: TextFormField(
+                                        controller: _controllers[index],
+                                        validator: (value) {
+                                          if (value == null || value.isEmpty) {
+                                            return 'Enter a name';
+                                          }
+                                          return null;
+                                        },
+                                        maxLength: 10,
+                                        decoration: InputDecoration(
+                                          border: OutlineInputBorder(
+                                            borderSide: BorderSide.none,
+                                            borderRadius: BorderRadius.circular(10), // Adjust border radius
                                           ),
-                                          SizedBox(width: 20),
-                                          Container(
-                                            decoration: BoxDecoration(
-                                              color: Colors.red,
-                                              borderRadius: BorderRadius.all(Radius.circular(10)),
-                                            ),
-                                            width: 48,
-                                            height: 48,
-                                            child: IconButton(
-                                              onPressed: () {
-                                                _numbersOfPlayers--;
-                                                players.removeAt(index);
-                                                setState(() {});
-                                              },
-                                              icon: Icon(size: 25, Icons.remove, color: Colors.white),
-                                            ),
-                                          ),
-                                        ],
+                                          fillColor: Colors.grey.shade200,
+                                          filled: true,
+                                          counterText: "",
+                                          hintText: 'Enter a name',
+                                          hintStyle: TextStyle(color: Colors.grey),
+                                          // Optional: to use the primary color for the cursor and text
+                                        ),
+                                        style: TextStyle(color: Colors.black),
                                       ),
                                     ),
                                   );
                                 },
                               ),
+                              SizedBox(height: 20),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  SizedBox(width: 10),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.red,
+                                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                                    ),
+                                    width: 48,
+                                    height: 48,
+                                    child: IconButton(
+                                      onPressed: removePlayer,
+                                      icon: Icon(size: 25, Icons.remove, color: Colors.white),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ],
                           ),
-                        )
+                        ),
                     ],
                   ),
                 ),
               ),
             ),
+            SizedBox(height: 20),
+            if (_numbersOfPlayers > 1)
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+                width: MediaQuery.of(context).size.width * .9,
+                height: 48,
+                child: TextButton(
+                  onPressed: StartGame,
+                  child: Text("START", style: TextStyle(color: Colors.white, fontSize: 20)),
+                ),
+              ),
           ],
         ),
       ),
